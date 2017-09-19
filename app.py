2b97bf69104e30507ccdf39d4848565ca4ee1352
LarsGammelgaard <lars.gammelgaard@gmail.com>
2017-09-19 23:11:57 +0200

    Lecture 47 - Menu.py added

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
 from database import Database
-from models.blog import Blog
+from menu import Menu
 
 __author__ = 'larsg'
 
 Database.initialize()
 
-blog = Blog(author='Jose',
-            title='Sample title',
-            description='Sample Description')
+menu = Menu()
 
-blog.new_post()
-
-blog.save_to_mongo()
-
-from_database = Blog.from_mongo(blog.id)
-
-print(blog.get_posts())
+menu.run_menu()
